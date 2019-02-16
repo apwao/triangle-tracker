a0d3e27ca7f42fa60c99f89a5cce2f9d7b26dadb
Sowayi <sapwao@gmail.com>
2019-02-16 17:42:03 -0500

    Add function to check for equilateral triangle

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,15 @@
-function triangleCheck(){
-// Obtain the three Dimensions
-let firstDimension = parseInt(document.getElementById("dimension1").value);
-let secondDimension = parseInt(document.getElementById("dimension2").value);
-let thirdDimension = parseInt(document.getElementById("dimension3").value);
-}
-// Discard the non-triangles
-if ((firstDimension + secondDimension) <= thirdDimension || (secondDimension + thirdDimension) <= firstDimension){
-	alert("Dimensions cannot form a triangle");
-
-}
-else if((firstDimension + thirdDimension) <= secondDimension){
+/Obtain input from user
+var firstDimension = parseInt(prompt("Enter First Dimension"));
+var secondDimension = parseInt(prompt("Enter Second Dimension"));
+var thirdDimension = parseInt(prompt("Enter Third Dimension"));
+//Discard dimensions that cannot form a triangle
+if ((firstDimension + secondDimension) === thirdDimension || (secondDimension + thirdDimension) === firstDimension || (firstDimension + thirdDimension) === secondDimension){
 	alert("Dimensions cannot form a triangle");
 }
-//Check for equilateral triangle
+else if((firstDimension + thirdDimension) < secondDimension || (firstDimension + secondDimension) < thirdDimension || (secondDimension + thirdDimension) < firstDimension){
+alert("Dimensions cannot form a triangle");
+}
+//Check for an equilateral triangle
 else if(firstDimension === secondDimension && secondDimension === thirdDimension){
 	alert("The dimensions form an equilateral Triangle");
-}
-//Check for scalene triangle
-else if(firstDimension !== secondDimension && secondDimension !== thirdDimension && firstDimension !== thirdDimension){
-	alert("The dimensions form a scalene triangle");
-}
-//Check for isoceles triangle
-else{
-alert("The dimensions form an isoceles triangle");
 }
