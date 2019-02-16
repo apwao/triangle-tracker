3521b0ebc5306a37eb2aa8c21beb9bfa76345fe6
Sowayi <sapwao@gmail.com>
2019-02-16 17:35:03 -0500

    Add the function to dismiss dimensions that cannot form a triangle

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,26 @@
-function validateinput(){
-  var firstDimension = document.getElementById('firstDimension');
-  var valid = true;
-  if firstDimension.value.length -- 0){
-    firstDimension.className = "invalid input";
-    firstDimension.getElementSibling.innerHTML = "dimension can't be blank";
-    valid = false}
-    return valid;
-  }
+function triangleCheck(){
+// Obtain the three Dimensions
+let firstDimension = parseInt(document.getElementById("dimension1").value);
+let secondDimension = parseInt(document.getElementById("dimension2").value);
+let thirdDimension = parseInt(document.getElementById("dimension3").value);
+}
+// Discard the non-triangles
+if ((firstDimension + secondDimension) <= thirdDimension || (secondDimension + thirdDimension) <= firstDimension){
+	alert("Dimensions cannot form a triangle");
+
+}
+else if((firstDimension + thirdDimension) <= secondDimension){
+	alert("Dimensions cannot form a triangle");
+}
+//Check for equilateral triangle
+else if(firstDimension === secondDimension && secondDimension === thirdDimension){
+	alert("The dimensions form an equilateral Triangle");
+}
+//Check for scalene triangle
+else if(firstDimension !== secondDimension && secondDimension !== thirdDimension && firstDimension !== thirdDimension){
+	alert("The dimensions form a scalene triangle");
+}
+//Check for isoceles triangle
+else{
+alert("The dimensions form an isoceles triangle");
 }
