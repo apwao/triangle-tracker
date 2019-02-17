9c697d4930d6ff87d4583f3fa76b2234144f9ded
Sowayi <sapwao@gmail.com>
2019-02-17 04:19:28 -0500

    Add function to obtain form from the document

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,14 @@
-/Obtain input from user
-var firstDimension = parseInt(prompt("Enter First Dimension"));
-var secondDimension = parseInt(prompt("Enter Second Dimension"));
-var thirdDimension = parseInt(prompt("Enter Third Dimension"));
+//Obtain form element from document
+let myForm = document.forms.checkerForm;
+//Obtain error message elements
+let errorMessage = document.getElementsByClassName("error");
+//Obtain input from user
+function triangleCheck(){
+// Obtain the three Dimensions
+let firstDimension = parseInt(document.getElementById("dimension1").value);
+let secondDimension = parseInt(document.getElementById("dimension2").value);
+let thirdDimension = parseInt(document.getElementById("dimension3").value);
+}
 //Discard dimensions that cannot form a triangle
 if ((firstDimension + secondDimension) === thirdDimension || (secondDimension + thirdDimension) === firstDimension || (firstDimension + thirdDimension) === secondDimension){
 	alert("Dimensions cannot form a triangle");
